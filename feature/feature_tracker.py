cdacaf61025fc104159fff8c79576bd6187aaa20
Tianshu <thetianshuhuang@gmail.com>
2018-04-09 14:47:07 -0500

    Basic algorithm works.

--- feature/feature_tracker.py
+++ feature/feature_tracker.py
@@ -2,6 +2,7 @@
 # feature identification based tracker
 
 import cv2
+import math
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -31,6 +32,36 @@ def weighted_grayscale(image, weight):
         split_image[1] * (weight[1] / 255.) / 3 +
         split_image[2] * (weight[2] / 255.) / 3
     ))
+
+
+def dnorm(vector, **kwargs):
+
+    """
+    Compute the discrete Lp norm of a vector.
+
+    Parameters
+    ----------
+    vector : float[]
+        Input vector
+    lp= : float
+        Norm to compute. Defaults to 2
+
+    Returns
+    -------
+    float
+        Computed norm
+    """
+
+    if("lp" in kwargs):
+        lp = kwargs["lp"]
+    else:
+        lp = 2
+
+    norm = 0
+    for coord in vector:
+        norm += math.pow(abs(coord), lp)
+
+    return(math.pow(norm, 1.0 / lp))
 
 
 def image_k_means(image, k):
@@ -81,10 +112,13 @@ def test_func(img_target, img_scene):
     # Need to draw only good matches, so create a mask
     matchesMask = [[0, 0] for i in xrange(len(matches))]
 
+    output = []
     # ratio test as per Lowe's paper
     for i, (m, n) in enumerate(matches):
-        if m.distance < 0.7 * n.distance:
+        if m.distance < 0.8 * n.distance:
             matchesMask[i] = [1, 0]
+            output.append([m, n])
+
     draw_params = dict(matchColor=(0, 255, 0),
                        singlePointColor=(255, 0, 0),
                        matchesMask=matchesMask,
@@ -93,17 +127,47 @@ def test_func(img_target, img_scene):
         img_target, kp1, img_scene, kp2, matches, None, **draw_params)
     plt.imshow(img3, ), plt.show()
 
-    for i in range(10):
-        print(matches[i][0].trainIdx)
-        print(matches[i][1].trainIdx)
+    match_vectors = {
+        "target": [kp1[match[0].queryIdx].pt for match in output],
+        "scene": [kp2[match[0].trainIdx].pt for match in output],
+        "weights": [match[0].distance for match in output],
+        "length": len(output)
+    }
+
+    return(match_vectors)
 
 
-target = cv2.imread("target.jpg")
-scene = cv2.imread("scene.jpg")
+target = cv2.imread("target2.jpg")
+scene = cv2.imread("scene2.jpg")
 weights = image_k_means(target, 1)
 
 print(weights)
-test_func(
+match_vectors = test_func(
     weighted_grayscale(target, weights[0]),
     weighted_grayscale(scene, weights[0]))
-#test_func(cv2.imread("target.jpg", 0), cv2.imread("scene.jpg", 0))
+# test_func(cv2.imread("target.jpg", 0), cv2.imread("scene.jpg", 0))
+
+target = match_vectors["target"]
+scene = match_vectors["scene"]
+length = match_vectors["length"]
+weights = match_vectors["weights"]
+
+x = []
+y = []
+w = []
+hist = []
+for i in range(length):
+    for j in range(i, length):
+        dx = dnorm([target[i][0] - target[j][0], target[i][1] - target[j][1]])
+        dy = dnorm([scene[i][0] - scene[j][0], scene[i][1] - scene[j][1]])
+        x.append(dx)
+        y.append(dy)
+        if(dy / (dx + 1) < 20):
+            hist.append(dy / (dx + 1))
+            w.append(1 / (weights[i] * weights[j]))
+
+plt.scatter(x, y)
+plt.show()
+
+plt.hist(hist, bins=100, weights=w)
+plt.show()
